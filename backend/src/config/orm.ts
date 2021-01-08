14af131cc3dab84ef77270b31dec29c12f60b3a8
Josimar Martins <josimarjr479@gmail.com>
2021-01-08 12:28:52 -0300

    :zap: adicionado configuraçao do typeorm

---
+++ backend/src/config/orm.ts
@@ -0,0 +1,12 @@
+import path from 'path';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const options: TypeOrmModuleOptions = {
+  type: 'sqlite',
+  database: 'data/mural_mensagens.db',
+  logging: true,
+  entities: [path.resolve(__dirname, '..', 'db', 'entities', '*')],
+  migrations: [path.resolve(__dirname, '..', 'db', 'migrations', '*')],
+}
+
+module.exports = options;
---
+++ backend/src/db/migrations/1610118839340-CreateUsers.ts
@@ -0,0 +1,12 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export default class CreateUsers1610118839340 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
