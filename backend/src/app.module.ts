dcba3444f8d866f8c843fe25ab65b9b5a599203a
Josimar Martins <josimarjr479@gmail.com>
2021-01-11 17:33:33 -0300

    :zap: atualizado arquivo de configuração no app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import * as ormOptions from './config/orm';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(ormOptions)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
