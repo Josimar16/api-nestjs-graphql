8240f85413bc70e5c95ce2b26da8458662d484e7
Josimar Martins <josimarjr479@gmail.com>
2021-01-11 17:33:02 -0300

    :zap: atualizado arquivo de configuração

--- backend/src/config/orm.ts
+++ backend/src/config/orm.ts
@@ -1,12 +1,12 @@
-import path from 'path';
+import * as path from 'path';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const options: TypeOrmModuleOptions = {
   type: 'sqlite',
   database: 'data/mural_mensagens.db',
   logging: true,
-  entities: [path.resolve(__dirname, '..', 'db', 'entities', '*')],
-  migrations: [path.resolve(__dirname, '..', 'db', 'migrations', '*')],
+  entities: [path.resolve(__dirname, '..', 'db', 'entities', '*.ts')],
+  migrations: [path.resolve(__dirname, '..', 'db', 'migrations', '*.ts')],
 }
 
 module.exports = options;
